d7c0c2b5fe70f6cfebf50e482da3dc5509ee0a4e
zietzm <michael.zietz@gmail.com>
2018-12-04 18:44:02 -0500

    Update relative import for extension module

--- xswap/xswap.py
+++ xswap/xswap.py
@@ -1,10 +1,10 @@
-import _xswap_backend
+import xswap._xswap_backend
 
 
 def permute_edge_list(edge_list, directed=False, multiplier=10, excluded_pair_set=set(), seed=0, log=False):
     max_source = max([i[0] for i in edge_list])
     max_target = max([i[1] for i in edge_list])
     num_swaps = int(10 * len(edge_list))
-    return _xswap_backend._xswap(edge_list, list(excluded_pair_set),
-                                 max_source, max_target, directed,
-                                 num_swaps, seed)
+    return xswap._xswap_backend._xswap(edge_list, list(excluded_pair_set),
+                                       max_source, max_target, directed,
+                                       num_swaps, seed)
