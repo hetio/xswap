b8ff1e63fd13c61cc19f3ca708079af0214a2880
zietzm <michael.zietz@gmail.com>
2018-12-07 14:50:42 -0500

    Test integration of LaTeX in README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # xswap
+
+## Usage examples
+
+## Choice of parameters
+
+### `directed` and `bipartite`
+
+The `bipartite` argument determines the meaning of a node's value.
+A bipartite graph is a graph in which nodes can be divided into disjoint sets with connections exclusively between sets.
+For example, consider the graph shown in the figure below:
+
+<img src="docs/img/bipartite_graph.png" alt="Image of bipartite graph" width="50%">
+
+The adjacency matrix corresponding to a bipartite graph can be broken into four blocks.
+
+<img src="https://latex.codecogs.com/gif.latex?A&space;&=&space;\begin{bmatrix}&space;0&space;&&space;B&space;\\&space;B^T&space;&&space;0&space;\end{bmatrix}" title="Bipartite graph adjacency matrix" />
+
+The two diagonal blocks are entirely zero and the two off-diagonal blocks are the biadjacency matrix and its transpose.
+
+<img src="https://latex.codecogs.com/gif.latex?B&space;&=&space;\begin{bmatrix}&space;1&space;&&space;0&space;&&space;0&space;&&space;0&space;\\&space;0&space;&&space;1&space;&&space;1&space;&&space;0&space;\\&space;0&space;&&space;0&space;&&space;0&space;&&space;1&space;\end{bmatrix}" title="Bipartite graph biadjacency matrix" />
+
+The biadjacency matrix is, in general, non-square.
+This means that the edge (0, 0) is not a self loop, as a 0 in the first position refers to a different node than a 0 in the second position.
--- xswap/xswap.py
+++ xswap/xswap.py
@@ -5,8 +5,7 @@ import xswap._xswap_backend
 
 def permute_edge_list(edge_list: List[Tuple[int, int]], directed: bool = False,
                       bipartite: bool = True, multiplier: float = 10,
-                      excluded_edges: Set[Tuple[int, int]] = set(),
-                      seed: int = 0, log: bool = False):
+                      excluded_edges: Set[Tuple[int, int]] = set(), seed: int = 0):
     """
     Permute the edges of a graph using the XSwap method given by Hanhijärvi,
     et al. (doi.org/f3mn58). XSwap is a degree-preserving network randomization
@@ -19,23 +18,29 @@ def permute_edge_list(edge_list: List[Tuple[int, int]], directed: bool = False,
     edge_list : List[Tuple[int, int]]
         Edge list representing the graph to be randomized. Tuples can contain
         integer values representing nodes. No value should be greater than C++'s
-        INT_MAX, in this case 2_147_483_647.
+        `INT_MAX`, in this case 2_147_483_647.
     directed : bool
-        Whether (0, 1) and (1, 0) should be treated as different edges. directed
-        should be False when (0, 1) is equivalent to (1, 0). In this case, the
+        Whether (0, 1) and (1, 0) should be treated as different edges. `directed`
+        should be `False` when (0, 1) is equivalent to (1, 0). In this case, the
         edges will be pruned so that only one copy of every edge is present.
         After XSwap is complete, then, copies of every new edge will be made so
         that the permuted edges have the same properties as the original edges.
     bipartite : bool
         Whether the set of source nodes is the same as the set of target nodes.
-        If (0, 0) is a self loop, then set bipartite=False. To permute a
+        If (0, 0) is a self loop, then set `bipartite=False`. To permute a
         bipartite graph, use only the edges going from one set of nodes to the
         other. That is, only use the edges found in the biadjacency matrix. For
         a more detailed explanation, see README.md.
     multiplier : float
+        The number of edge swap attempts is determined by the product of the
+        number of existing edges and multiplier. For example, if five edges are
+        passed and multiplier is set to 10, 50 swaps will be attempted. Non-integer
+        products will be rounded down to the nearest integer.
     excluded_edges : Set[Tuple[int, int]]
+        Specific edges which should never be created by the network randomization
     seed : int
-    log : bool
+        Random seed that will be passed to the C++ Mersenne Twister 19937 random
+        number generator.
     """
     if directed and bipartite:
         raise NotImplementedError("Directed bipartite graphs are not supported.")
