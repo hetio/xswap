da8c1b54d300d85f18842da6d86c5004fa389d06
Michael Zietz <michael.zietz@gmail.com>
2019-06-17 10:22:55 -0400

    Fix package management

--- xswap/__init__.py
+++ xswap/__init__.py
@@ -1,9 +1,12 @@
-from xswap.xswap import permute_edge_list
+from xswap import network_formats
 from xswap import preprocessing
+from xswap.xswap import permute_edge_list
 
 __version__ = '0.0.2'
 
 __all__ = [
+    network_formats.edges_to_matrix,
+    network_formats.matrix_to_edges,
     permute_edge_list,
     preprocessing.load_str_edges,
     preprocessing.load_processed_edges,
